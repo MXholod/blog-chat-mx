bbe9e14aa58d5aaa7085bb09b802ec93c95f50a6
Michael Holod <git_ara@ukr.net>
2020-08-24 17:33:11 +0300

    The beginning of the application structure looks like that

---
+++ pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    {{ test }}
+  </div>
+</template>
+<script>
+export default {
+  async asyncData ({ $http }) {
+    const test = await $http.$get('/api/test')
+    return {
+      test
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
