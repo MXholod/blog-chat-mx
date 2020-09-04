c9343dd224fb9b7451c0328fef6c48857cf98930
Michael Holod <git_ara@ukr.net>
2020-09-04 19:18:47 +0300

    Base layout for site was created

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,14 @@
 <template>
-  <div>
-    <Nuxt />
-  </div>
+  <el-container>
+    <el-header>Header</el-header>
+    <el-container>
+      <el-aside width="200px">Aside</el-aside>
+      <el-main>
+        <Nuxt />
+      </el-main>
+    </el-container>
+    <el-footer>Footer</el-footer>
+  </el-container>
 </template>
 
 <style>
