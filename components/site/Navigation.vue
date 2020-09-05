57a8b0ecc5d11c3e7b95c62accbd2cdbe06b7c9c
Michael Holod <git_ara@ukr.net>
2020-09-05 22:57:03 +0300

    Header is almost completed

---
+++ components/site/Navigation.vue
@@ -0,0 +1,81 @@
+<template>
+    <nav>
+        <el-menu
+            :default-active="activeIndex1"
+            class="el-menu-sport"
+            mode="horizontal"
+            @select="handleSelect"
+            background-color="#225AA4"
+            text-color="#fff"
+            active-text-color="#12DEF1">
+            <el-menu-item index="1" class="el-menu-sport__item" >Sportsman blog</el-menu-item>
+            <el-submenu index="2" class="el-menu-sport__item_sub">
+                <template slot="title">A sportsman life</template>
+                <el-submenu index="2-1">
+                    <template slot="title">My nutrition</template>
+                    <el-menu-item index="2-1-1">Everyday nutrition</el-menu-item>
+                    <el-menu-item index="2-1-2">Nutrition during exercise</el-menu-item>
+                </el-submenu>
+                <el-submenu index="2-2">
+                    <template slot="title">A set of exercises</template>
+                    <el-menu-item index="2-2-1">Morning warm-up exercises</el-menu-item>
+                    <el-menu-item index="2-2-2">Strength exercises</el-menu-item>
+                    <el-menu-item index="2-2-3">Agility exercises</el-menu-item>
+                </el-submenu>
+            </el-submenu>
+            <el-menu-item index="3" class="el-menu-sport__item">About me</el-menu-item>
+            <el-menu-item index="4" class="el-menu-sport__item">Contacts</el-menu-item>
+            <el-menu-item index="5" class="el-menu-sport__item el-menu-sport__item_hover"></el-menu-item>
+            <el-menu-item index="6" class="el-menu-sport__item" disabled>Teammates chat</el-menu-item>
+        </el-menu>
+    </nav>
+</template>
+
+<script>
+/* eslint-disable */
+  export default {
+    data() {
+      return {
+        activeIndex1: '2'
+      };
+    },
+    methods: {
+      handleSelect(key, keyPath) {
+        console.log(key, keyPath);
+      }
+    }
+  }
+</script>
+
+<style lang="scss">
+    .el-menu-sport{
+      height:80px;
+      display:flex;
+      flex-direction: row;
+    }
+    .el-menu-sport__item{
+      height:100% !important;
+      font-size:1.2rem;
+      flex-grow: 1;
+    }
+    .el-menu-sport__item_hover{
+      flex-grow: 12;
+      background: transparent url('/Places.png') 50% 0/160px 80px no-repeat padding-box border-box scroll; 
+      &:hover{
+        background-color: transparent !important;
+      }  
+    }
+    .el-menu-sport__item_sub{
+      flex-grow: 1; 
+      & div.el-submenu__title{
+        height:78px !important;
+        font-size:1.2rem;
+      }
+    }
+    @mixin aaa($c:violet){
+        h1{
+            color: $c;
+        }
+    }
+    @include aaa(green);
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,15 +1,26 @@
 <template>
   <el-container>
-    <el-header>Header</el-header>
+    <el-header height="80px" class="el-header-base-layout">
+      <Navigation />
+    </el-header>
     <el-container>
       <el-aside width="200px">Aside</el-aside>
       <el-main>
         <Nuxt />
       </el-main>
     </el-container>
-    <el-footer>Footer</el-footer>
+    <el-footer class="el-footer-base-layout">Footer</el-footer>
   </el-container>
 </template>
+
+<script>
+import Navigation from './../components/site/Navigation'
+export default {
+  components: {
+    Navigation
+  }
+}
+</script>
 
 <style>
 html {
@@ -29,5 +40,12 @@ html {
   -moz-osx-font-smoothing: grayscale;
   -webkit-font-smoothing: antialiased;
   box-sizing: border-box;
+}
+.el-header-base-layout{
+  padding:0;
+}
+.el-footer-base-layout{
+  padding:0;
+  background-color:#225AA4;
 }
 </style>
