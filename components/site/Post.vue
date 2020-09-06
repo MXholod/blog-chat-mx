d6d98a334389248a90f3565dd0ebdce55026270e
Michael Holod <git_ara@ukr.net>
2020-09-06 22:36:21 +0300

    Posts are displayed on main page (with a test information)

---
+++ components/site/Post.vue
@@ -0,0 +1,68 @@
+<template>
+    <el-card
+        shadow="hover"
+        :body-style="{padding: 0}"
+        class="post">
+        <header slot="header" class="post__header">
+            <h1>Post title</h1>
+            <small>
+                <i class="el-icon-time"></i>
+                {{ new Date().toLocaleString() }}
+            </small>
+        </header>
+        <div class="post__body">
+            <img
+                src="/posts_img/sport_balls.jpg"
+                alt="Post image"
+                class="post__img"
+                />
+        </div>
+        <footer class="post__footer">
+            <el-button round @click="openPost">Open post</el-button>
+            <span>
+                <i class="el-icon-message"></i>
+                12
+            </span>
+        </footer>
+    </el-card>
+</template>
+
+<script>
+export default {
+  methods: {
+    openPost () {
+      console.log('Post')
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+    .post{
+        min-height:200px;
+        border: 1px solid #000;
+        margin-bottom: 2rem;
+        .post__header{
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+            small{
+                text-align: center;
+            }
+        }
+        .post__body{
+            background-color: #f6f6f6;
+            //margin-bottom: 1rem;
+            .post__img{
+                width: 100%;
+                min-height: auto;
+            }
+        }
+        .post__footer{
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+            padding: 0.5rem;
+        }
+    }
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,12 @@
 <template>
   <div>
-    <v-app dark>
-      <v-layout column justify-center align-center>
-        <v-flex xs12 sm8 md6>
-          <v-btn @click="message" color="info">
-            Message
-          </v-btn>
-        </v-flex>
-      </v-layout>
-    </v-app>
+    <el-row type="flex" justify="center">
+      <el-col :xs="24" :sm="18" :md="12" :lg="10">
+        <blog-post
+          v-for="post in 3"
+          :key="post" />
+      </el-col>
+    </el-row>
     {{ test }}
     <hr>
     <el-button type="success">
@@ -26,7 +24,14 @@
   </div>
 </template>
 <script>
+import BlogPost from '@/components/site/Post'
 export default {
+  components: {
+    BlogPost
+  },
+  head: {
+    title: 'Main sport page'
+  },
   sockets: {
     connect () {
       console.log('socket connected')
