bc34dc730ffc14f2834dcc2cd8875f368e4869ad
Michael Holod <git_ara@ukr.net>
2020-09-29 22:37:11 +0300

    Admin layout and index page have been created

---
+++ layouts/admin.vue
@@ -0,0 +1,37 @@
+<template>
+    <el-container class="el-wrapper-base-layout">
+        <el-header height="80px" class="el-header-base-layout">
+            Header
+        </el-header>
+        <el-container>
+            <el-aside class="el-aside-base-layout">Aside</el-aside>
+            <el-main>
+                <Nuxt />
+            </el-main>
+        </el-container>
+        <el-footer class="el-footer-base-layout">Footer</el-footer>
+    </el-container>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="scss" scoped>
+.el-wrapper-base-layout{
+  height: 100vh;
+}
+.el-header-base-layout{
+  padding:0;
+  background-color:#225AA4;
+}
+.el-aside-base-layout{
+    width: 250px !important;
+    background-color:#77AAED;
+}
+.el-footer-base-layout{
+  padding:0;
+  background-color:#225AA4;
+}
+</style>
