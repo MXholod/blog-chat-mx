cf827a0943d5d769b40ad7507a272d80fc3168a7
Michael Holod <git_ara@ukr.net>
2020-09-30 17:30:02 +0300

    Pages for admin are created

---
+++ pages/admin/chat/room.vue
@@ -0,0 +1,9 @@
+<template>
+    <h1>Room management</h1>
+</template>
+
+<script>
+export default {
+  layout: 'admin'
+}
+</script>
---
+++ pages/admin/chat/rooms.vue
@@ -0,0 +1,9 @@
+<template>
+    <h1>List of rooms</h1>
+</template>
+
+<script>
+export default {
+  layout: 'admin'
+}
+</script>
