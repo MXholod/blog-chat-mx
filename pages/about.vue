0ffd887297c89dc9ec8383a76c7ed6e3406bb1fd
Michael Holod <git_ara@ukr.net>
2020-10-14 23:51:02 +0300

    Two static pages “contacts” and “about” have been created

---
+++ pages/about.vue
@@ -0,0 +1,20 @@
+<template>
+  <el-row type="flex" justify="center">
+    <el-col :xs="24" :sm="18" :md="12" :lg="10" :xl="24">
+      <h2>About</h2>
+    </el-col>
+  </el-row>
+</template>
+<script>
+export default {
+  head: {
+    title: 'About'
+  },
+  methods: {
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ pages/contacts.vue
@@ -0,0 +1,20 @@
+<template>
+  <el-row type="flex" justify="center">
+    <el-col :xs="24" :sm="18" :md="12" :lg="10" :xl="24">
+      <h2>Contacts</h2>
+    </el-col>
+  </el-row>
+</template>
+<script>
+export default {
+  head: {
+    title: 'Contacts'
+  },
+  methods: {
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
