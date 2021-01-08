4e8f48010a3b8821d9cfb91e831214e592e7dd81
Michael Holod <git_ara@ukr.net>
2021-01-08 22:33:11 +0200

    Model of 'User' is modified along with DB settings moved to a separate 'db.js' Helper file

---
+++ api/helpers/db.js
@@ -0,0 +1,30 @@
+// Mongoose for MongoDB
+const mongoose = require('mongoose');
+const User = require('./../models/user');
+// Config keys
+const keys = require('./../config/keys');
+
+function connectDB(){
+  //Mongoose. mongoose.connect() returns Promise()
+  mongoose.connect(keys.MONGO_DB_URI, {
+    'useCreateIndex': true,
+    'useNewUrlParser': true ,
+    'useFindAndModify': false,
+    'useUnifiedTopology': true
+  })
+  .then(() => {
+    console.log('MongoDB connected...')
+  })
+  .catch((error) => console.log('MongoDB error ', error))
+}
+
+//The function to check existing ID
+function isValidId(id) {
+  return mongoose.Types.ObjectId.isValid(id);
+}
+
+module.exports = {
+  connectDB, //Make a DB connection
+  User,
+  isValidId
+};
--- api/index.js
+++ api/index.js
@@ -1,52 +1,42 @@
-const { app, http } = require('./api.socket')
+//app - express, http - httpServer
+const { app, http } = require('./api.socket');
+//Mongoose and MongoDB
+const { connectDB } = require('./helpers/db');
 // Parse incoming request bodies in a middleware before handlers, available under the req.body property
-const bodyParser = require('body-parser')
-// Mongoose for MongoDB
-const mongoose = require('mongoose')
+const bodyParser = require('body-parser');
 // Passport library for restricting Routes
-const passport = require('passport')
-const strategyPassport = require('./middleware/passport-strategy')
-// Config keys
-const keys = require('./config/keys')
+const passport = require('passport');
+const strategyPassport = require('./middleware/passport-strategy');
+//CORS
+const cors = require('cors');
+app.use(cors());
 
 // Require API routes
-const auth = require('./routes/auth')
-const post = require('./routes/post')
-const comment = require('./routes/comment')
+//const auth = require('./routes/auth');
+const post = require('./routes/post');
+const comment = require('./routes/comment');
 
-// mongoose.connect() returns Promise()
-mongoose.connect(keys.MONGO_DB_URI, {
-  'useCreateIndex': true, 
-  'useNewUrlParser': true ,
-  'useFindAndModify': false,
-  'useUnifiedTopology': true
-  })
-  .then(() => {
-    console.log('MongoDB connected...')
-  })
-  .catch((error) => console.log('MongoDB error ', error))
+//Mongo connection
+connectDB();
 // Register Passport. If User failed a Route gets the 401 status
-app.use(passport.initialize())
-passport.use(strategyPassport)
+//app.use(passport.initialize());
+//passport.use(strategyPassport);
 // Register Body-Parser
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
 // Import API Routes
-app.use('/auth',auth)
-app.use('/post',post)
-app.use('/comment',comment)
+//app.use('/auth',auth);
+app.use('/post',post);
+app.use('/comment',comment);
 
-// Export express app
-module.exports = app
+//Create server for Socket.IO
+const port = process.env.PORT_API || 3001;
+http.listen(port, () => {
+  console.log(`API server listening on port`)
+})
 
-// Start standalone server if directly running  
-// if (require.main === module) { 
-  const port = process.env.PORT_API || 3001 
-  if(parseInt(process.env.HTTP_SERVER_API_SOCKET) === 1){
-      process.env.HTTP_SERVER_API_SOCKET = 0
-      // Start socket server
-      http.listen(port, () => {
-        console.log(`API server listening on port ${port}`)
-      })
-  }
-// } 
+export default {
+  // path: '/api',
+  handler: app
+}
