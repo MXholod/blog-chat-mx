adceb75cb870b4f83f12251224e04c9d8ba57991
Michael Holod <git_ara@ukr.net>
2021-01-19 17:42:31 +0200

    User email verification Route is ready to use

--- api/routes/user.js
+++ api/routes/user.js
@@ -2,8 +2,12 @@ const { Router } = require('express')
 const router = Router()
 const validationRegistration = require('./../middleware/registration-user');
 const { register } = require('./../controllers/register');
+const { emailVerification } = require('./../controllers/email-verification');
 
-//First time registration
+//First time registration '/api/user/registration'
 router.post('/registration', validationRegistration(), register);
+//Email verification
+//'/api/user/verification'
+router.post('/verification', (req,res,next)=>next(), emailVerification);
 
 module.exports = router
