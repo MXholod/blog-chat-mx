0665f0a82467c6d8c8c63cb3e619335e8a2f9a59
Michael Holod <git_ara@ukr.net>
2021-01-31 17:42:16 +0200

    Helper for creating 'Refresh token' was done that lives for a week

---
+++ api/helpers/jwt-refresh.js
@@ -0,0 +1,14 @@
+const { JwtRefresh } = require('./../helpers/db');
+const { randomTokenString } = require('./../helpers/crypto');
+
+function generateRefreshToken(user, ipAddress) {
+    // Create a refresh token that expires in 7 days
+    return new JwtRefresh({
+        user: user.id,
+        token: randomTokenString(),
+        expires: new Date(Date.now() + 7*24*60*60*1000),
+        createdByIp: ipAddress
+    });
+}
+
+module.exports = generateRefreshToken;
