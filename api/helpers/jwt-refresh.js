8bfce09729618ec4e2436f35cb8b29e8b2d6b1d2
Michael Holod <git_ara@ukr.net>
2021-02-03 00:34:48 +0200

    'Refresh token' helper has been modified to avoid duplicates in database

--- api/helpers/jwt-refresh.js
+++ api/helpers/jwt-refresh.js
@@ -2,13 +2,18 @@ const { JwtRefresh } = require('./../helpers/db');
 const { randomTokenString } = require('./../helpers/crypto');
 
 function generateRefreshToken(user, ipAddress) {
-    // Create a refresh token that expires in 7 days
-    return new JwtRefresh({
-        user: user.id,
-        token: randomTokenString(),
-        expires: new Date(Date.now() + 7*24*60*60*1000),
-        createdByIp: ipAddress
-    });
+  JwtRefresh.deleteOne({ user: user.id }, function(err){
+    if(err){
+      throw new Error("Token refresh exception");
+    }
+  });
+  // Create a refresh token that expires in 7 days
+  return new JwtRefresh({
+    user: user.id,
+    token: randomTokenString(),
+    expires: new Date(Date.now() + 7*24*60*60*1000),
+    createdByIp: ipAddress
+  });
 }
 
 module.exports = generateRefreshToken;
