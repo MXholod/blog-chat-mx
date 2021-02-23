5d3cc1120b33e111b6b25a1f66019c0fef966189
Michael Holod <git_ara@ukr.net>
2021-02-23 19:38:42 +0200

    Axios file as a plugin for Nuxt has been created that allows apply data to the requests and responses  thanks to axios interceptors

--- plugins/axios-handler.js
+++ plugins/axios-handler.js
@@ -1,14 +1,51 @@
-export default function({ $axios, redirect, store }){
-  $axios.onError(error => {
-    if(error.response){
-      //401 Unauthorized,  this sends with 'WWW-Authenticate' that contains information about authorization
-      if(error.response.status === 401){
-        store.dispatch('auth/logout');
-        redirect('/login?message=session');
-      }
-      if(error.response.status === 500){
-        console.error('Server Error is 500');
-      }
+export default function({ $axios, store, redirect }){
+  $axios.interceptors.request.use(req =>{
+    if(store.getters['auth/isUserAuthenticated'].jwtToken && !req.headers.common['Authorization']){
+      req.headers.common['Authorization'] = `Bearer ${store.getters['auth/isUserAuthenticated'].jwtToken}`;
     }
+    return req;
+  }, error => {
+    // handle the error
+    return Promise.reject(error);
+  });
+
+  const interceptor = $axios.interceptors.response.use(response => {
+      if(process.server) {
+        $axios.interceptors.response.eject(interceptor);
+      }
+      return response;
+  }, error => {
+    if(process.server) {
+      $axios.interceptors.response.eject(interceptor);
+    }
+    // When Refresh-Token expired
+    if (error.response.status === 401 && error.response.data.message) {
+      store.commit('auth/userLogOut',{root: true});
+      store.commit('error/setError','Your session is up',{root: true});
+      redirect('/login');
+    }
+    // When Access-Token expired
+    if (error.response.status === 401 && !error.response.data.message) {
+      //store.dispatch('auth/refreshToken');
+      $axios.$post('/api/auth/user/refresh').then(data =>{
+        if(data){
+          const { details : { jwtToken : activeToken } } = data;
+          return activeToken;
+        }
+        throw data;
+      }).then(activeToken => {
+        $axios.defaults.headers.common['Authorization'] = `Bearer ${activeToken}`;
+        store.commit('error/setError',
+          {jwtRefreshed : 'Your session is extended, have a nice work!'},
+          {root: true}
+        );
+        return error.config;
+      }).catch(err => {
+        return error;
+      });
+      return error.config;
+    }
+    return error;
+    //return Promise.reject(error);
   });
 }
