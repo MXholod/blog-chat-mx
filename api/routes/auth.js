17e03dc7281662ed16715a7d712d9caad2dd884d
Michael Holod <git_ara@ukr.net>
2021-02-24 00:13:20 +0200

    The route to get a new pair of 'access' and 'refresh' tokens is registered

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -2,13 +2,15 @@ const { Router } = require('express');
 const passport = require('passport');
 const loginUser = require('./../middleware/login-user');
 const { authenticate } = require('../controllers/auth');
+const { refresh_token } = require('../controllers/jwt-refresh');
 const router = Router();
 
 // When work with Login Authorization in REST api use post() method
 // REST API: get() - get data; post() - create data; put() - change/update data; delete() - delete data;
 // /api/auth/user/login
 router.post('/user/login', loginUser(), authenticate);
-
+// '/api/auth/user/refresh'
+router.post('/user/refresh', refresh_token);
 // /api/auth/admin/create
 /*router.post(
   '/admin/create',
