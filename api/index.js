7e8d0b3ce7636ae1bd2ce9179945acead2905de8
Michael Holod <git_ara@ukr.net>
2021-02-24 00:27:53 +0200

    Jwt passport cabinet middleware, cookie-parser and route to refresh tokens are registered

--- api/index.js
+++ api/index.js
@@ -4,31 +4,39 @@ const { app, http } = require('./api.socket');
 const { connectDB } = require('./helpers/db');
 // Parse incoming request bodies in a middleware before handlers, available under the req.body property
 const bodyParser = require('body-parser');
+// Parse cookies and add them to the Request object
+const cookieParser = require('cookie-parser');
 // Passport library for restricting Routes
 const passport = require('passport');
-const strategyPassport = require('./middleware/passport-strategy');
+const strategyJwtPassport = require('./middleware/user-jwt-auth');
 //CORS
 const cors = require('cors');
 app.use(cors());
 
 // Require API routes
-//const auth = require('./routes/auth');
+const auth = require('./routes/auth');
+const user = require('./routes/user');
 const post = require('./routes/post');
 const comment = require('./routes/comment');
+const cabinet = require('./routes/user-cabinet');
 
 //Mongo connection
 connectDB();
 // Register Passport. If User failed a Route gets the 401 status
-//app.use(passport.initialize());
-//passport.use(strategyPassport);
+app.use(passport.initialize());
+passport.use('jwt-cabinet',strategyJwtPassport);
 // Register Body-Parser
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+//Register Cookie-parser
+app.use(cookieParser());
 
 // Import API Routes
-//app.use('/auth',auth);
+app.use('/auth',auth);
+app.use('/user',user);
 app.use('/post',post);
 app.use('/comment',comment);
+app.use('/cabinet',cabinet);
 
 //Create server for Socket.IO
 const port = process.env.PORT_API || 3001;
