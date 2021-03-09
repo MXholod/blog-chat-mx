0399d5f546c896557b14d84e47f20314d9d7a338
Michael Holod <git_ara@ukr.net>
2021-03-09 21:51:27 +0200

    API route to update user name (login) is completed

--- api/routes/user-cabinet.js
+++ api/routes/user-cabinet.js
@@ -1,10 +1,14 @@
 const { Router } = require('express');
 const passport = require('passport');
-const test = require('./../controllers/user-cabinet');
+//const test = require('./../controllers/user-cabinet');
 const authCabinet = passport.authenticate('jwt-cabinet', {session: false});
+const userLoginMiddleware = require('./../middleware/update-user-login');
+const { updateUserLogin } = require('./../controllers/user-cabinet');
 const router = Router();
 
 // /api/cabinet/test
-router.get('/test', authCabinet, test);
+//router.get('/test', authCabinet, test);
+// /api/cabinet/user-name
+router.put('/user-name', authCabinet, userLoginMiddleware(), updateUserLogin);
 
 module.exports = router;
