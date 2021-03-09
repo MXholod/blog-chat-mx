43a76ac52da0f42fba22a42919693fd7ad444412
Michael Holod <git_ara@ukr.net>
2021-03-09 22:01:21 +0200

    Controller API has been completed, it save new user name (login) into DB

--- api/controllers/user-cabinet.js
+++ api/controllers/user-cabinet.js
@@ -1,6 +1,25 @@
-function test(req, res,next){
+const { validationResult } = require('express-validator');
+const { User } = require('./../helpers/db');
+
+/*function test(req, res,next){
   //console.log("Auth token ",req.headers.authorization," ",req.user);
   return res.status(200).json({ message: "Ok" });
+}*/
+async function updateUserLogin(req, res, next){
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ message: "Your new login is incorrect"});
+  }
+  try{
+    //Find ID in DB and then update user login
+    const result = await User.findOneAndUpdate({_id: req.user.id},{$set:{ login: req.body.newLogin }},{new:true});
+    return res.status(200).json({ message: "You've chaged your login", userName: result.login });
+  }catch(e){
+    return res.status(400).json({ message: "Error "+e.message});
+  }
 }
 
-module.exports = test;
+module.exports = {
+  //test,
+  updateUserLogin
+};
