a61add551509582efa247ca0f1ea60015cb542c4
Michael Holod <git_ara@ukr.net>
2021-03-12 23:44:49 +0200

    API route to update user password is completed

--- api/routes/user-cabinet.js
+++ api/routes/user-cabinet.js
@@ -4,11 +4,15 @@ const passport = require('passport');
 const authCabinet = passport.authenticate('jwt-cabinet', {session: false});
 const userLoginMiddleware = require('./../middleware/update-user-login');
 const { updateUserLogin } = require('./../controllers/user-cabinet');
+const userPasswordMiddleware = require('./../middleware/update-user-password');
+const { updateUserPassword } = require('./../controllers/user-cabinet');
 const router = Router();
 
 // /api/cabinet/test
 //router.get('/test', authCabinet, test);
 // /api/cabinet/user-name
 router.put('/user-name', authCabinet, userLoginMiddleware(), updateUserLogin);
+// /api/cabinet/user-password
+router.put('/user-password', authCabinet, userPasswordMiddleware(), updateUserPassword);
 
 module.exports = router;
