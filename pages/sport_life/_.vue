cd0d56e20754297fafa49226c6b34f4e794e9966
Michael Holod <git_ara@ukr.net>
2021-03-17 01:33:11 +0200

    Folder 'sport_life' to render unknown depth of pages has been created with a special file '_.vue' in it

---
+++ pages/sport_life/_.vue
@@ -0,0 +1,44 @@
+<template>
+  <section class="container">
+    <div>
+      <h1 class="title">
+        {{ title }}
+      </h1>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  data(){
+    return { title: "Users VUE" }
+  },
+  head () {
+    return {
+      title: `Test: ${this.title}`
+    }
+  },
+  mounted() {
+    console.log("ID ",this.$route.params);
+  }
+}
+</script>
+
+<style scoped>
+.container {
+  /*margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;*/
+}
+.title
+{
+  margin-top: 30px;
+}
+.header
+{
+  margin-top: 30px;
+}
+</style>
