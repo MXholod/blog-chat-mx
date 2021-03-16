b88ec2ea2d4187edafd3931d6bb478735738bc61
Michael Holod <git_ara@ukr.net>
2021-03-17 01:47:04 +0200

    There are two components that create recursive menu for the 'sport_life' directory and based on the 'Element UI'

---
+++ components/site/navigation_recursion/ChildItem.vue
@@ -0,0 +1,29 @@
+<template>
+  <el-submenu v-if="child && child.childrenItems.length" :index="'/sport_life/'+child.id">
+    <template slot="title">{{ child.pageName }}</template>
+    <el-menu-item  :index="'/sport_life/'+child.id">
+      {{ child.pageName +'( Full list)'}}
+    </el-menu-item>
+    <node
+      v-for="curChild in child.childrenItems"
+      :key="curChild.id"
+      :child="curChild"
+      :index="'/sport_life/'+curChild.id">
+      {{ curChild.pageName+' Test' }}
+    </node>
+  </el-submenu>
+  <el-menu-item v-else :index="'/sport_life/'+child.id">
+    {{ child.pageName }}
+  </el-menu-item>
+</template>
+
+<script>
+export default {
+  name: "node",
+  props:{ child: Object },
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ components/site/navigation_recursion/ParentItem.vue
@@ -0,0 +1,24 @@
+<template>
+  <div v-if="parentData">
+    <div v-for="parentDataItem in parentData" :key="parentDataItem.id">
+      <child-item v-if="parentDataItem.childrenItems.length !== 0" :child="parentDataItem"></child-item>
+      <el-menu-item v-else :index="'/sport_life/'+parentDataItem.id">
+        {{ parentDataItem.pageName }}
+      </el-menu-item>
+    </div>
+  </div>
+</template>
+
+<script>
+import ChildItem from '@/components/site/navigation_recursion/ChildItem';
+export default {
+  props:{ parentData: Array },
+  components:{
+    ChildItem
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
