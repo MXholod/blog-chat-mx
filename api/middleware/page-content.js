ba788a077b3618455e24e46e56545e03a4bd82fa
Michael Holod <git_ara@ukr.net>
2021-04-15 02:33:03 +0300

    A nested pages route has got a middleware to check a URL parameter

---
+++ api/middleware/page-content.js
@@ -0,0 +1,16 @@
+const { param } = require('express-validator');
+
+function validationParamPageContent(){
+  return [
+    param('pageContent').custom((value)=>{
+      const reg = /^[A-Za-z0-9-]{0,36}$/g;
+      if(!reg.test(value)){
+        throw new Error('Page that you are looking for not found');
+      }
+      // Indicates the success of this synchronous custom validator
+      return true;
+    })
+  ];
+}
+
+module.exports = validationParamPageContent;
