d75a5c700e5e04ead23f6365eae402096c067b30
Michael Holod <git_ara@ukr.net>
2021-04-15 02:27:11 +0300

    A specific page can be get by the nested pages route

--- api/routes/menu-page.js
+++ api/routes/menu-page.js
@@ -1,9 +1,12 @@
 const { Router } = require('express');
 const router = Router();
-const { getMenuPages, createPage } = require('./../controllers/menu-page');
+const validationParamPageContent = require('./../middleware/page-content');
+const { getMenuPages, getMenuPageContent, createPage } = require('./../controllers/menu-page');
 
 // '/api/menu_page/page'
 router.get('/page', getMenuPages);
+// '/api/menu_page/page/:reference'
+router.get('/page/:reference', validationParamPageContent(), getMenuPageContent);
 // '/api/menu_page/create'
 router.post('/create', createPage);
 
