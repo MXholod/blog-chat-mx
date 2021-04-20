443a431e45c53f61071abad6685e87cfe0fd8ba8
Michael Holod <git_ara@ukr.net>
2021-04-20 18:26:46 +0300

    The middleware has been added to the route for menu page item creation and now we can upload an image to the server

--- api/routes/menu-page.js
+++ api/routes/menu-page.js
@@ -1,6 +1,7 @@
 const { Router } = require('express');
 const router = Router();
 const validationParamPageContent = require('./../middleware/page-content');
+const upload = require('./../middleware/file-uploads');
 const { getMenuPages, getMenuPageContent, createPage } = require('./../controllers/menu-page');
 
 // '/api/menu_page/page'
@@ -8,6 +9,6 @@ router.get('/page', getMenuPages);
 // '/api/menu_page/page/:reference'
 router.get('/page/:reference', validationParamPageContent(), getMenuPageContent);
 // '/api/menu_page/create'
-router.post('/create', createPage);
+router.post('/create', upload.single('singleImage'), createPage);
 
 module.exports = router;
