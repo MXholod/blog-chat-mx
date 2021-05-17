980faae7ba8fba862fbbc1885f5dcf864dead855
Michael Holod <git_ara@ukr.net>
2021-05-17 23:40:03 +0300

    'Menu page' Route is ready to update content

--- api/routes/menu-page.js
+++ api/routes/menu-page.js
@@ -5,15 +5,17 @@ const authCabinetAdmin = passport.authenticate('jwt-cabinet-admin', {session: fa
 const validationParamPageContent = require('./../middleware/page-content');
 const upload = require('./../middleware/file-uploads');
 const uploadOptimized = require('./../middleware/file-uploads-optimized');
-const { getMenuPages, getMenuPageContent, createPage, getFullPageContent } = require('./../controllers/menu-page');
+const { getMenuPages, getMenuPageContent, createPage, getFullPageContent, updatePage } = require('./../controllers/menu-page');
 
 // '/api/menu_page/page'
 router.get('/page', getMenuPages);
 // '/api/menu_page/page/:reference'
 router.get('/page/:reference', validationParamPageContent(), getMenuPageContent);
 // '/api/menu_page/create'
-router.post('/create',authCabinetAdmin ,upload.single('singleImage'), uploadOptimized, createPage);
+router.post('/create', authCabinetAdmin, upload.single('singleImage'), uploadOptimized, createPage);
 // '/api/menu_page/full_page/:reference'
 router.get('/full_page/:reference', validationParamPageContent(), getFullPageContent);
+// 'api/menu_page/update'
+router.put('/update', authCabinetAdmin, upload.single('singleImage'), uploadOptimized, updatePage);
 
 module.exports = router;
