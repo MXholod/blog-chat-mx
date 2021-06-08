f53ebb590b0bc26de52dff1ab2c46701b4ae223e
Michael Holod <git_ara@ukr.net>
2021-06-09 01:53:51 +0300

    Page 'user.vue' in admin panel has been protected by the function from plugin

--- pages/admin/user.vue
+++ pages/admin/user.vue
@@ -1,12 +1,47 @@
 <template>
-  <admin-user-management />
+  <admin-user-management v-if="showUsers" />
 </template>
 
 <script>
 import AdminUserManagement from '@/components/admin/AdminUserManagement'
 export default {
+  async asyncData(context){
+    let jwt = context.store.getters['auth/isUserAuthenticated'].jwtToken;
+    //'$isAllowedByRole' is a function from Plugin
+    let userData = await context.app.$isAllowedByRole(jwt);
+    if(userData.role === 'guest'){
+      context.redirect('/cabinet');
+    }
+    if(userData.role === '' && !userData.sessionEnd){
+      if(context.store.getters['auth/isUserAuthenticated'].role !== ''){
+        context.store.dispatch('auth/logout');
+      }
+      context.redirect('/');
+    }
+    let showUsers = false;
+    //If we have role: 'admin' or 'moderator'
+    if(userData.role && !userData.sessionEnd){
+      showUsers = true;
+    }
+    return {
+      userLogoutRefresh: userData.sessionEnd ? true : false,
+      showUsers
+    };
+  },
   layout: 'admin',
-  middleware: ['admin-auth'],
-  components: { AdminUserManagement }
+  components: { AdminUserManagement },
+  mounted(){
+    //Displaying the modal window to inform user about the end of the session
+    if(this.userLogoutRefresh){
+      this.$alert('Your session is up!', 'Session state', {
+        confirmButtonText: 'Sign in again',
+        showClose:false,
+        callback: action => {
+          this.$store.dispatch('auth/logout');
+          this.$router.push('/login?message=unauthenticated');
+        }
+      });
+    }
+  }
 }
 </script>
