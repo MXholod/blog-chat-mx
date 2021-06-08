ad2cd92c8bf271469b77aa276af18012a872d1e0
Michael Holod <git_ara@ukr.net>
2021-06-09 00:11:02 +0300

    Page 'index.vue' in admin panel has been protected by the function from plugin

--- pages/admin/index.vue
+++ pages/admin/index.vue
@@ -10,21 +10,36 @@ import { mapState } from 'vuex'
 export default {
   async asyncData(context){
     let jwt = context.store.getters['auth/isUserAuthenticated'].jwtToken;
+    //'$isAllowedByRole' is a function from Plugin
     let userData = await context.app.$isAllowedByRole(jwt);
-
     if(userData.role === 'guest'){
       context.redirect('/cabinet');
     }
-    if(userData.role === ''){
+    if(userData.role === '' && !userData.sessionEnd){
       if(context.store.getters['auth/isUserAuthenticated'].role !== ''){
         context.store.dispatch('auth/logout');
       }
       context.redirect('/');
     }
-    return { jwt };
+    return {
+      userLogoutRefresh: userData.sessionEnd ? true : false
+    };
   },
   layout: 'admin',
   name: 'admin',
   //computed: mapState('auth', ['testDataUser'])
+  mounted(){
+    //Displaying the modal window to inform user about the end of the session
+    if(this.userLogoutRefresh){
+      this.$alert('Your session is up!', 'Session state', {
+        confirmButtonText: 'Sign in again',
+        showClose:false,
+        callback: action => {
+          this.$store.dispatch('auth/logout');
+          this.$router.push('/login?message=unauthenticated');
+        }
+      });
+    }
+  }
 }
 </script>
