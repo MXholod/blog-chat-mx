0b37104c1f60885e245cda0c5b54ca8eb832c561
Michael Holod <git_ara@ukr.net>
2021-06-09 01:05:57 +0300

    Page 'create.vue' in admin panel has been protected by the function from plugin

--- pages/admin/create.vue
+++ pages/admin/create.vue
@@ -63,8 +63,24 @@
 
 <script>
 export default {
+  async asyncData(context){
+    let jwt = context.store.getters['auth/isUserAuthenticated'].jwtToken;
+    //'$isAllowedByRole' is a function from Plugin
+    let userData = await context.app.$isAllowedByRole(jwt);
+    if(userData.role === 'guest'){
+      context.redirect('/cabinet');
+    }
+    if(userData.role === '' && !userData.sessionEnd){
+      if(context.store.getters['auth/isUserAuthenticated'].role !== ''){
+        context.store.dispatch('auth/logout');
+      }
+      context.redirect('/');
+    }
+    return {
+      userLogoutRefresh: userData.sessionEnd ? true : false
+    };
+  },
   layout: 'admin',
-  middleware: ['user-auth-admin'],
   data () {
     return {
       activeName: 'first',
@@ -124,6 +140,19 @@ export default {
     },
     handleChangeImage (file, fileList) {
       this.postImage = file.raw
+    }
+  },
+  mounted(){
+    //Displaying the modal window to inform user about the end of the session
+    if(this.userLogoutRefresh){
+      this.$alert('Your session is up!', 'Session state', {
+        confirmButtonText: 'Sign in again',
+        showClose:false,
+        callback: action => {
+          this.$store.dispatch('auth/logout');
+          this.$router.push('/login?message=unauthenticated');
+        }
+      });
     }
   }
 }
