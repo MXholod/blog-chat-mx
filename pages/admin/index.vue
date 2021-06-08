875fc92e89845719983735765fddf2579eeff06b
Michael Holod <git_ara@ukr.net>
2021-06-08 18:07:29 +0300

    It is allowed to visit the Main admin page for admin and moderator only

--- pages/admin/index.vue
+++ pages/admin/index.vue
@@ -1,10 +1,30 @@
 <template>
-    <h1>Welcome to admin page</h1>
+    <div>
+      <h1>Welcome to admin page</h1>
+      <NuxtLink to="/">Home</NuxtLink>
+    </div>
 </template>
 
 <script>
+import { mapState } from 'vuex'
 export default {
+  async asyncData(context){
+    let jwt = context.store.getters['auth/isUserAuthenticated'].jwtToken;
+    let userData = await context.app.$isAllowedByRole(jwt);
+
+    if(userData.role === 'guest'){
+      context.redirect('/cabinet');
+    }
+    if(userData.role === ''){
+      if(context.store.getters['auth/isUserAuthenticated'].role !== ''){
+        context.store.dispatch('auth/logout');
+      }
+      context.redirect('/');
+    }
+    return { jwt };
+  },
   layout: 'admin',
-  middleware: ['admin-auth']
+  name: 'admin',
+  //computed: mapState('auth', ['testDataUser'])
 }
 </script>
