205ff4a6a300009b43ef302962756191da1cdc94
Michael Holod <git_ara@ukr.net>
2021-06-18 15:00:32 +0300

    The Route that creates a comment for the Post works good

--- api/routes/comment.js
+++ api/routes/comment.js
@@ -1,8 +1,10 @@
-const { Router } = require('express')
-const { createComment } = require('./../controllers/comment')
-const router = Router()
+const { Router } = require('express');
+const passport = require('passport');
+const authCabinet = passport.authenticate('jwt-cabinet', {session: false});
+const { createComment } = require('./../controllers/comment');
+const router = Router();
 
-// /api/comment
-router.post('/', createComment)
+// '/api/comment'
+router.post('/', authCabinet, createComment);
 
-module.exports = router
+module.exports = router;
