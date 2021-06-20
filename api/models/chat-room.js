6929f96189cd7b859c4cce6ae729e5f9028b0fc7
Michael Holod <git_ara@ukr.net>
2021-06-21 00:06:16 +0300

    Model for the chat room has been created

---
+++ api/models/chat-room.js
@@ -0,0 +1,16 @@
+/* eslint-disable */
+const { Schema, model } = require('mongoose')
+
+const chatRoomSchema = new Schema({
+  name: { type: String, required: true },
+  description: { type: String, required: true },
+  /*
+  // Relationship with 'users' - one-to-many
+  user: { type: Schema.Types.ObjectId, ref: 'User' },
+  // Relationship with 'chat-messages' - many-to-many
+  messages: [
+    { type: Schema.Types.ObjectId, ref: 'Chat-message' }
+  ]*/
+})
+
+module.exports = model('Chat-room', chatRoomSchema)
