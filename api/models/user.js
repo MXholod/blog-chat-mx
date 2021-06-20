23d7587ee5c881c7fcd5065f87174044a9e6e49a
Michael Holod <git_ara@ukr.net>
2021-06-21 00:19:59 +0300

    User Model was corrected

--- api/models/user.js
+++ api/models/user.js
@@ -22,7 +22,7 @@ const UserSchema = new Schema({
   // Relationship with 'posts' - one-to-many
   posts: [ { type: Schema.Types.ObjectId, ref: 'Post' } ],
   // Relationship with 'chat-rooms' - one-to-many
-  rooms: [ { type: Schema.Types.ObjectId, ref: 'Chat-room' } ],
+  //rooms: [ { type: Schema.Types.ObjectId, ref: 'Chat-room' } ],
   registeredByAdmin: { type: Boolean, default: false }
 });
 
