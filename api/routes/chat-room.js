f3ff401a59e6d20a60d223eace5d3a72ff0485f1
Michael Holod <git_ara@ukr.net>
2021-06-21 22:36:45 +0300

    The Route for getting all the rooms of the chat

--- api/routes/chat-room.js
+++ api/routes/chat-room.js
@@ -2,9 +2,12 @@ const { Router } = require('express');
 const router = Router();
 const passport = require('passport');
 const authCabinetAdmin = passport.authenticate('jwt-cabinet-admin', {session: false});
-const { createRoom } = require('./../controllers/chat-room');
+const authCabinet = passport.authenticate('jwt-cabinet', {session: false});
+const { createRoom, getAllRooms } = require('./../controllers/chat-room');
 
 // '/api/chat_room/create'
 router.post('/create', authCabinetAdmin, createRoom);
+// '/api/chat_room/rooms'
+router.get('/rooms', authCabinet, getAllRooms);
 
 module.exports = router;
