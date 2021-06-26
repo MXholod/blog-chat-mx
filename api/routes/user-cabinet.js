331ce267313997fc541be8758d82012cff303569
Michael Holod <git_ara@ukr.net>
2021-06-26 22:29:36 +0300

    Route that allows to add and update an avatar

--- api/routes/user-cabinet.js
+++ api/routes/user-cabinet.js
@@ -1,18 +1,26 @@
 const { Router } = require('express');
 const passport = require('passport');
-//const test = require('./../controllers/user-cabinet');
 const authCabinet = passport.authenticate('jwt-cabinet', {session: false});
 const userLoginMiddleware = require('./../middleware/update-user-login');
-const { updateUserLogin } = require('./../controllers/user-cabinet');
+const {
+  updateUserLogin,
+  updateUserPassword,
+  updateUserAvatar
+} = require('./../controllers/user-cabinet');
 const userPasswordMiddleware = require('./../middleware/update-user-password');
-const { updateUserPassword } = require('./../controllers/user-cabinet');
+const upload = require('./../middleware/avatar-upload');
+const uploadOptimized = require('./../middleware/avatar-upload-optimize');
 const router = Router();
 
-// /api/cabinet/test
-//router.get('/test', authCabinet, test);
 // /api/cabinet/user-name
 router.put('/user-name', authCabinet, userLoginMiddleware(), updateUserLogin);
 // /api/cabinet/user-password
 router.put('/user-password', authCabinet, userPasswordMiddleware(), updateUserPassword);
+// /api/cabinet/user-avatar/update
+router.patch('/user-avatar/update',
+  authCabinet,
+  upload.single('singleImage'),
+  uploadOptimized,
+  updateUserAvatar);
 
 module.exports = router;
