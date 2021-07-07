a6bd8e69be181f1c9cd3daba80a12e85addb707e
Michael Holod <git_ara@ukr.net>
2021-07-07 23:09:27 +0300

    The 'chat-message' Model is created to save a chat message

---
+++ api/models/chat-message.js
@@ -0,0 +1,18 @@
+/* eslint-disable */
+const { Schema, model } = require('mongoose')
+
+const chatMessageSchema = new Schema({
+  text: { type: String, required: true },
+  date: { type: Date, default: Date.now },
+  divider: { type: Boolean, default: false },
+  inset: { type: Boolean, default: false },
+  user: { type: Schema.Types.ObjectId, ref: 'User' },
+  room: { type: Schema.Types.ObjectId, ref: 'Chat-room' }
+  /*
+  // Relationship with 'chat-rooms' - many-to-many
+  rooms: [
+    { type: Schema.Types.ObjectId, ref: 'Chat-room' }
+  ]*/
+})
+
+module.exports = model('Chat-message', chatMessageSchema)
