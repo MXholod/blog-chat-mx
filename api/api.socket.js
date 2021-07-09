85be53d4239805548fad5238355d37c2b835d4e2
Michael Holod <git_ara@ukr.net>
2021-07-09 17:00:35 +0300

    An event of Socket 'createMessage' on the server side using property 'userName' to save user's message in DB

--- api/api.socket.js
+++ api/api.socket.js
@@ -65,7 +65,8 @@ io.on('connection', socket => {
         ChatMessage.create({
           text: data.text,
           user: data.userId,
-          room: data.roomId
+          room: data.roomId,
+          userName: user.name
         }, function (err,success) {
           if(err){
               //console.log("Error ",err);
