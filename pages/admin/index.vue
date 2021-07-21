ad38f62a72112227609e9d61e76cd15713ecfbd6
Michael Holod <git_ara@ukr.net>
2021-07-21 19:32:46 +0300

    The admin main page provides a panel to manage with the site logo

--- pages/admin/index.vue
+++ pages/admin/index.vue
@@ -1,12 +1,20 @@
 <template>
     <div>
-      <h1>Welcome to admin page</h1>
       <NuxtLink to="/">Home</NuxtLink>
+      <el-tabs type="card" @tab-click="handleTabs">
+        <el-tab-pane label="Admin info">
+          <h1>Welcome to the admin page</h1>
+        </el-tab-pane>
+        <el-tab-pane v-if="user.role === 'admin'" label="Site logo">
+          <logo :imageLink="logo" :key="reloadLogo" />
+        </el-tab-pane>
+      </el-tabs>
     </div>
 </template>
 
 <script>
 import { mapState } from 'vuex'
+import Logo from './../../components/admin/site_logo/Logo.vue';
 export default {
   async asyncData(context){
     let jwt = context.store.getters['auth/isUserAuthenticated'].jwtToken;
@@ -21,13 +29,38 @@ export default {
       }
       context.redirect('/');
     }
-    return {
-      userLogoutRefresh: userData.sessionEnd ? true : false
+    let siteOptions;
+    if(userData.role === 'admin'){
+      siteOptions = await context.app.$getAllSiteOptions(jwt);
+      if(siteOptions){
+        return {
+          userLogoutRefresh: userData.sessionEnd ? true : false,
+          logo: siteOptions?.options?.siteLogo
+        };
+      }
+    }else{
+      return {
+      userLogoutRefresh: userData.sessionEnd ? true : false,
+      logo: siteOptions
     };
+    }
   },
   layout: 'admin',
   name: 'admin',
-  //computed: mapState('auth', ['testDataUser'])
+  components: {
+    Logo
+  },
+  computed: mapState('auth', ['user']),
+  data(){
+    return {
+      activeName: 'first' ,
+      logo:'',
+      reloadLogo: 0
+    }
+  },
+  methods:{
+    async handleTabs(tab, event) {}
+  },
   mounted(){
     //Displaying the modal window to inform user about the end of the session
     if(this.userLogoutRefresh){
