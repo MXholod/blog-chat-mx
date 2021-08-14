67302d656911196065b359e030553893cdbba407
Michael Holod <git_ara@ukr.net>
2021-08-14 21:17:12 +0300

    Model 'chat-room' has been changed. Its property 'name' has got 'unique' to prevent duplicating

--- api/models/chat-room.js
+++ api/models/chat-room.js
@@ -2,7 +2,7 @@
 const { Schema, model } = require('mongoose')
 
 const chatRoomSchema = new Schema({
-  name: { type: String, required: true },
+  name: { type: String, required: true, unique: true },
   description: { type: String, required: true },
   /*
   // Relationship with 'users' - one-to-many
