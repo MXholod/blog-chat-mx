7b52edbaa831d9992c93fb6ae4d5460ee012d033
Michael Holod <git_ara@ukr.net>
2021-08-14 22:55:22 +0300

    Chat menu has been changed in admin section

--- components/admin/MenuAside.vue
+++ components/admin/MenuAside.vue
@@ -3,7 +3,7 @@
         active-text-color="#225AA4"
         router
         :default-active="$route.path"
-        class="admin-chat"
+        class="admin-menu"
     >
         <el-menu-item index="/admin">
             <i class="el-icon-notebook-2"></i>
@@ -29,22 +29,17 @@
             <i class="el-icon-circle-close"></i>
             <span>Exit</span>
         </el-menu-item>
-      <el-submenu index="/admin/chat">
+      <el-submenu index="/admin/chat" class="admin-chat">
         <template slot="title">
           <i class="el-icon-s-grid"></i>
-          <span>Chat management</span>
+          <span class="admin-chat__header">Chat management</span>
         </template>
         <el-menu-item-group class="admin-chat__title"  title="Chat rooms">
           <el-menu-item index="/admin/chat/rooms">List of rooms</el-menu-item>
           <el-menu-item index="/admin/chat/room">Creating a chat room</el-menu-item>
         </el-menu-item-group>
-        <el-menu-item-group class="admin-chat__title" title="Chat users">
-          <el-menu-item index="/admin/chat/users">Usres in the chat</el-menu-item>
-          <el-menu-item index="/admin/chat/user">User management</el-menu-item>
-        </el-menu-item-group>
         <el-menu-item-group class="admin-chat__title" title="Admin messages">
           <el-menu-item index="/admin/chat/messages">Message management</el-menu-item>
-          <el-menu-item index="/admin/chat/message">Admin message</el-menu-item>
         </el-menu-item-group>
       </el-submenu>
     </el-menu>
@@ -56,18 +51,30 @@ export default {
 </script>
 
 <style lang="scss">
-.admin-chat{
+.admin-menu{
   .el-menu-item.is-active{
     font-weight: bold;
     font-size: 1em;
   }
 }
-.admin-chat__title{
-  .el-menu-item-group__title{
-    color: #225AA4;
-    margin-left:10%;
-    font-size: .9em;
-    font-weight: bold;
+.admin-chat{
+  $color: #225aa4;
+  background-color: $color;
+  border-bottom:2px solid lighten($color, $amount: 10%);
+  .el-submenu__title:hover{
+    background-color: lighten($color, $amount: 10%);
+  }
+  .admin-chat__header{
+    color:#f6f6f6;
+  }
+  .admin-chat__title{
+    background-color: #b4d4f5;
+    .el-menu-item-group__title{
+      color: $color;
+      margin-left:-5%;
+      font-size: .9em;
+      font-weight: bold;
+    }
   }
 }
 </style>
