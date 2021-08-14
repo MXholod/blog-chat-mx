d949c7d473edf0f89e4a5ce9f8c2de5341318b66
Michael Holod <git_ara@ukr.net>
2021-08-14 21:10:44 +0300

    Controller has been changed. This controller creates a chat room that name will never duplicated in DB

--- api/controllers/chat-room.js
+++ api/controllers/chat-room.js
@@ -1,6 +1,11 @@
 const { ChatRoom } = require('./../helpers/db');
+const { validationResult } = require('express-validator');
 
 async function createRoom(req, res){
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ message: errors.array() });
+  }
   const newRoomData = req.body;
   try{
     const room = new ChatRoom(newRoomData);
