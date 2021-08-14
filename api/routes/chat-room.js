74795f34f68b86e36f0eb50e28e4abb5a90dfe47
Michael Holod <git_ara@ukr.net>
2021-08-14 20:54:07 +0300

    Route has been changed. This route creates a chat room that name will never duplicated in DB

--- api/routes/chat-room.js
+++ api/routes/chat-room.js
@@ -3,10 +3,11 @@ const router = Router();
 const passport = require('passport');
 const authCabinetAdmin = passport.authenticate('jwt-cabinet-admin', {session: false});
 const authCabinet = passport.authenticate('jwt-cabinet', {session: false});
+const chatRoomValidation = require('./../middleware/chat-room-validation');
 const { createRoom, getAllRooms } = require('./../controllers/chat-room');
 
 // '/api/chat_room/create'
-router.post('/create', authCabinetAdmin, createRoom);
+router.post('/create', authCabinetAdmin, chatRoomValidation(), createRoom);
 // '/api/chat_room/rooms'
 router.get('/rooms', authCabinet, getAllRooms);
 
