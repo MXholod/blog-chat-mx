15bfcf9a06e7df6967d9e2df54c3232d21367d09
Michael Holod <git_ara@ukr.net>
2021-08-18 00:43:48 +0300

    Routes for chat have been created in admin panel. With their help we can list all the rooms with messages and also delete them

--- api/routes/chat-room.js
+++ api/routes/chat-room.js
@@ -4,11 +4,20 @@ const passport = require('passport');
 const authCabinetAdmin = passport.authenticate('jwt-cabinet-admin', {session: false});
 const authCabinet = passport.authenticate('jwt-cabinet', {session: false});
 const chatRoomValidation = require('./../middleware/chat-room-validation');
-const { createRoom, getAllRooms } = require('./../controllers/chat-room');
+const {
+  createRoom,
+  getAllRooms,
+  getAllRoomsWithMessages,
+  deleteRoomWithMessages
+ } = require('./../controllers/chat-room');
 
 // '/api/chat_room/create'
 router.post('/create', authCabinetAdmin, chatRoomValidation(), createRoom);
 // '/api/chat_room/rooms'
 router.get('/rooms', authCabinet, getAllRooms);
+// '/api/chat_room/rooms_messages'
+router.get('/rooms_messages', authCabinetAdmin, getAllRoomsWithMessages);
+// '/api/chat_room/rooms_messages'
+router.delete('/rooms_messages', authCabinetAdmin, deleteRoomWithMessages);
 
 module.exports = router;
