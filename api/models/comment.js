30807af183367f5827787a8171889f35b35b8153
Michael Holod <git_ara@ukr.net>
2021-10-14 03:01:34 +0300

    The 'comment' model has been changed to store an 'id' of the user who adds a comment

--- api/models/comment.js
+++ api/models/comment.js
@@ -4,7 +4,8 @@ const commentSchema = new Schema({
   name: { type: String, required: true },
   text: { type: String, required: true },
   date: { type: Date, default: Date.now },
-  postId: { type: Schema.Types.ObjectId, ref: 'posts' }
+  postId: { type: Schema.Types.ObjectId, ref: 'posts' },
+  user: { type: Schema.Types.ObjectId, ref: 'User' }
 })
 
 module.exports = model('Comment',commentSchema)
