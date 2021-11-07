576c0cb1be942ae40cedc343d7b87d4a797a053b
Michael Holod <git_ara@ukr.net>
2021-11-08 00:34:47 +0200

    Route that displays sidebar on the site conditionally has been created

--- api/routes/sidebar.js
+++ api/routes/sidebar.js
@@ -4,12 +4,15 @@ const passport = require('passport');
 const authCabinetAdmin = passport.authenticate('jwt-cabinet-admin', {session: false});
 const {
   getSidebarVisibility,
-  changeSidebarVisibility
+  changeSidebarVisibility,
+  displaySidebar
  } = require('./../controllers/sidebar');
 
 // 'api/sidebar/admin/visibility'
 router.get('/admin/visibility',authCabinetAdmin, getSidebarVisibility);
 // 'api/sidebar/admin/visibility:state'
 router.patch('/admin/visibility/:state',authCabinetAdmin, changeSidebarVisibility);
+// 'api/sidebar/visibility'
+router.get('/visibility', displaySidebar);
 
 module.exports = router;
