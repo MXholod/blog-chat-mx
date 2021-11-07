73bfedc3e2e1f493ac5b6a487875ba0c02d45877
Michael Holod <git_ara@ukr.net>
2021-11-07 19:48:59 +0200

    The model 'Site-settings' has been changed. Several properties are added to this model for manipulation with the sidebar on the client side

--- api/models/site-settings.js
+++ api/models/site-settings.js
@@ -2,7 +2,16 @@ const { Schema, model } = require('mongoose');
 
 const schemaSiteSettings = new Schema({
   siteLogo: { type: String, default: '' },
-  adminId: { type: String, required: true }
+  adminId: { type: String, required: true },
+  sidebarVisibility: { type: Boolean, default: true },
+  popularPostsLimit: { type: Number, default: 0 },
+  popularPostsVisibility: { type: Boolean, default: true },
+  popularPagesLimit: { type: Number, default: 0 },
+  popularPagesVisibility: { type: Boolean, default: true },
+  recentlyCreatedPostsLimit: { type: Number, default: 0 },
+  recentlyCreatedPostsVisibility: { type: Boolean, default: true },
+  recentlyCreatedPagesLimit: { type: Number, default: 0 },
+  recentlyCreatedPagesVisibility: { type: Boolean, default: true }
 });
 
 
