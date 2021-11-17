9b8ea77c7e36bcaf4f4ae65371a4c6d7008baac2
Michael Holod <git_ara@ukr.net>
2021-11-18 00:19:12 +0200

    Routes that allow to manage with the blocks on the sidebar panel have been created. Those routes provide information about the blocks in admin panel

--- api/routes/sidebar.js
+++ api/routes/sidebar.js
@@ -3,16 +3,31 @@ const router = express.Router();
 const passport = require('passport');
 const authCabinetAdmin = passport.authenticate('jwt-cabinet-admin', {session: false});
 const {
-  getSidebarVisibility,
+  getSidebarSettings,
   changeSidebarVisibility,
-  displaySidebar
+  displaySidebar,
+  getPopularPosts,
+  getPopularPages,
+  getRecentlyCreatedPosts,
+  getRecentlyCreatedPages,
+  updateBlockSettings
  } = require('./../controllers/sidebar');
 
-// 'api/sidebar/admin/visibility'
-router.get('/admin/visibility',authCabinetAdmin, getSidebarVisibility);
+// 'api/sidebar/admin/settings'
+router.get('/admin/settings',authCabinetAdmin, getSidebarSettings);
 // 'api/sidebar/admin/visibility:state'
 router.patch('/admin/visibility/:state',authCabinetAdmin, changeSidebarVisibility);
 // 'api/sidebar/visibility'
 router.get('/visibility', displaySidebar);
+// 'api/sidebar/posts/popular_posts'
+router.get('/admin/posts/popular_posts',authCabinetAdmin, getPopularPosts);
+// 'api/sidebar/posts/popular_pages'
+router.get('/admin/pages/popular_pages',authCabinetAdmin, getPopularPages);
+// 'api/sidebar/posts/recent_posts'
+router.get('/admin/posts/recent_posts',authCabinetAdmin, getRecentlyCreatedPosts);
+// 'api/sidebar/posts/recent_pages'
+router.get('/admin/pages/recent_pages',authCabinetAdmin, getRecentlyCreatedPages);
+// 'api/sidebar/admin/block'
+router.put('/admin/block',authCabinetAdmin, updateBlockSettings);
 
 module.exports = router;
