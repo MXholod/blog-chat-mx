1c70dd31e0502376c9fffc8daa59091ab55b2818
Michael Holod <git_ara@ukr.net>
2022-01-03 03:14:57 +0200

    Routes for 'static-page' API have been created

---
+++ api/routes/static-page.js
@@ -0,0 +1,25 @@
+const { Router } = require('express');
+const router = Router();
+const passport = require('passport');
+const authCabinetAdmin = passport.authenticate('jwt-cabinet-admin', {session: false});
+const {
+  getAllStaticPages,
+  getStaticPage,
+  createStaticPage,
+  updateStaticPage,
+  deleteStaticPage } = require('./../controllers/static-page');
+
+// '/api/static_page/pages'
+router.get('/pages', getAllStaticPages);
+// '/api/static_page/admin/pages'
+router.get('/admin/pages', authCabinetAdmin, getAllStaticPages);
+// '/api/static_page/page/:pageName'
+router.get('/page/:pageName', getStaticPage);
+// '/api/static_page/admin/page'
+router.post('/admin/page', authCabinetAdmin, createStaticPage);
+// '/api/static_page/admin/page'
+router.put('/admin/page', authCabinetAdmin, updateStaticPage);
+// '/api/static_page/admin/page'
+router.delete('/admin/page', authCabinetAdmin, deleteStaticPage);
+
+module.exports = router;
