356237c3663d504587ba56458326895947a62540
Michael Holod <git_ara@ukr.net>
2022-01-11 00:57:30 +0200

    The controller 'static page' has got a new action. This action returns all the static pages that are allowed to display

--- api/controllers/static-page.js
+++ api/controllers/static-page.js
@@ -1,4 +1,4 @@
-const { StaticPage } = require('./../helpers/db');
+const { StaticPage, SiteSettings } = require('./../helpers/db');
 
 //For admin
 async function getAllStaticPages(req, res){
@@ -101,10 +101,56 @@ async function deleteStaticPage(req, res){
   }
 }
 
+async function getAllStaticPagesLimited(req, res){
+  try{
+    const staticPages = await StaticPage.find({});
+    const siteSettings = await SiteSettings.find({});
+    //Get all static pages and site settings
+    if(staticPages && siteSettings && siteSettings[0]){
+      const limit = siteSettings[0].staticPagesLimit;
+      //If pages are absent at all
+      if(staticPages.length === 0){
+        return res.status(200).json({ message: "Static pages are absent", static: [] });
+      }
+      //If pages more or equal than limit
+      if(staticPages.length >= limit){
+        let allStaticPages = staticPages.splice(0, limit);
+        allStaticPages = allStaticPages.map((staticPage) => {
+          //Capitalize first letter
+          let name = staticPage.name[0].toUpperCase()+staticPage.name.substr(1);
+          return {
+            name,
+            header: staticPage.header,
+            text: staticPage.text
+          }
+        });
+        return res.status(200).json({ message: "Pages more than limit", static: allStaticPages });
+      }
+      //That's the collision
+      if(staticPages.length < limit){
+        let allStaticPages = staticPages.splice(0, staticPages.length);
+        allStaticPages = allStaticPages.map((staticPage) => {
+          //Capitalize first letter
+          let name = staticPage.name[0].toUpperCase()+staticPage.name.substr(1);
+          return {
+            name,
+            header: staticPage.header,
+            text: staticPage.text
+          }
+        });
+        return res.status(200).json({ message: "Pages less than limit", static: allStaticPages });
+      }
+    }
+  }catch(e){
+    return res.status(500).json({ message: "Can't get limit of static", static: null });
+  }
+}
+
 module.exports = {
   getAllStaticPages,
   getStaticPage,
   createStaticPage,
   updateStaticPage,
-  deleteStaticPage
+  deleteStaticPage,
+  getAllStaticPagesLimited
 }
